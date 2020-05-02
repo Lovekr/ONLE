17e68b8e7c1aea35c3831d8374caad970545b62d
Lovekr <41716800+Lovekr@users.noreply.github.com>
2020-05-02 16:31:44 +0530

    Add files via upload

--- thirdeye_onle.sql
+++ thirdeye_onle.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 02, 2020 at 09:29 AM
+-- Generation Time: May 02, 2020 at 01:01 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.4
 
@@ -51,7 +51,28 @@ INSERT INTO `sms_admin` (`id`, `name`, `username`, `loginpwd`, `email_id`, `crea
 (7, 'Tours &amp; Travels', 'unwrap_blo1', 'S0sxOUFsK2haMy9RK1JnZ0lyWTF2Zz09', 'admin@gmail.com', '2020-05-02 04:32:28', '2020-05-02 04:32:28'),
 (8, 'WOODEN FURNITURES', 'unwrap_blogs4', 'S0sxOUFsK2haMy9RK1JnZ0lyWTF2Zz09', 'admin@gmail.com', '2020-05-02 06:54:03', '2020-05-02 06:54:03'),
 (9, 'prasanna', 'prjoshi', 'dGRkeWEwNmcxeDEwQzJ6NnZ1VWlndz09', 'prjoshi1992@gmail.com', '2020-05-02 07:10:00', '2020-05-02 07:10:00'),
-(10, 'prasanna', 'joshi_joshi', 'dGRkeWEwNmcxeDEwQzJ6NnZ1VWlndz09', 'pra.jps@gmail.com', '2020-05-02 07:17:20', '2020-05-02 07:17:20');
+(10, 'prasanna', 'joshi_joshi', 'dGRkeWEwNmcxeDEwQzJ6NnZ1VWlndz09', 'pra.jps@gmail.com', '2020-05-02 07:17:20', '2020-05-02 07:17:20'),
+(11, 'AI', 'ggggh', 'L042bTZ3YjhuN3BGZGxic2t4YW9aUT09', 'admin@gmail.com', '2020-05-02 10:59:20', '2020-05-02 10:59:20');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sms_auth_tokens`
+--
+
+CREATE TABLE `sms_auth_tokens` (
+  `id` int(11) NOT NULL,
+  `token` text COLLATE utf8_unicode_ci NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `sms_auth_tokens`
+--
+
+INSERT INTO `sms_auth_tokens` (`id`, `token`, `user_id`, `created_date`) VALUES
+(131, '675ca647d967c4f6740e33b1687add8bf3091f02d5622ab200d910ed3e34b7045331d838ac13cd95def2cb7618e6576053619008fd8e748a8e9ce6199e3eb742', 11, '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -110,6 +131,12 @@ ALTER TABLE `sms_admin`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `sms_auth_tokens`
+--
+ALTER TABLE `sms_auth_tokens`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `sms_create_meeting`
 --
 ALTER TABLE `sms_create_meeting`
@@ -129,7 +156,13 @@ ALTER TABLE `sms_login_log`
 -- AUTO_INCREMENT for table `sms_admin`
 --
 ALTER TABLE `sms_admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `sms_auth_tokens`
+--
+ALTER TABLE `sms_auth_tokens`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=132;
 
 --
 -- AUTO_INCREMENT for table `sms_create_meeting`
